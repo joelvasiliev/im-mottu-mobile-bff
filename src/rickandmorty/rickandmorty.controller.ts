d1cdda07f947e52e9326cfcd885ab332d93ff791
Joel Vasiliev <joel.vasiliev.neto@gmail.com>
2025-04-03 13:10:01 -0300

    fix: removido código inutilizado

--- src/rickandmorty/rickandmorty.controller.ts
+++ src/rickandmorty/rickandmorty.controller.ts
@@ -12,33 +12,6 @@ export class RickandmortyController {
   @ApiOkResponse({
     description: 'Retornou com sucesso um personagem aleatório',
     type: Character,
-    content: {
-      'application/json': {
-        example: {
-          id: 826,
-          name: 'Butter Robot',
-          status: 'unknown',
-          species: 'Robot',
-          type: '',
-          gender: 'unknown',
-          origin: {
-            name: 'unknown',
-            url: '',
-          },
-          location: {
-            name: 'Earth (Replacement Dimension)',
-            url: 'https://rickandmortyapi.com/api/location/20',
-          },
-          image: 'https://rickandmortyapi.com/api/character/avatar/826.jpeg',
-          episode: [
-            'https://rickandmortyapi.com/api/episode/10',
-            'https://rickandmortyapi.com/api/episode/22',
-          ],
-          url: 'https://rickandmortyapi.com/api/character/826',
-          created: '2021-11-01T12:34:56.789Z',
-        },
-      },
-    },
   })
   @ApiResponse({
     status: 500,
